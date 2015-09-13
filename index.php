0b1f23c7bbaed875da869095b44a4d7aa04048e9
PhilipJames <pjkelnhofer@gmail.com>
2015-09-13 12:12:53 -0400

    Add some content; adjusted styling

--- index.php
+++ index.php
@@ -24,10 +24,10 @@
     <div class="container">
       <div class="header clearfix">
         <nav>
-          <ul class="nav nav-pills pull-right">
+          <ul class="nav nav-pills pull-right"><!-- 
             <li role="presentation" class="active"><a href="#">Home</a></li>
             <li role="presentation"><a href="#">About</a></li>
-            <li role="presentation"><a href="#">Contact</a></li>
+            <li role="presentation"><a href="#">Contact</a></li> -->
           </ul>
         </nav>
         <h3 class="text-muted">Philip Kelnhofer</h3>
@@ -38,33 +38,43 @@
         <h1>DWA-15 Portfolio</h1>
         <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
       </div>
+      <div class="row">
+        <div class="col-lg-12 well">
+          <h3>Project Links</h3>
+          <ul>
+            <li>P2 - <a href="#">Website</a> - <a href="#">Github</a></li>
+            <li>P3 - <a href="#">Website</a> - <a href="#">Github</a></li>
+            <li>P4 - <a href="#">Website</a> - <a href="#">Github</a></li>
+          </ul>
+        </div>
 
+      </div>
       <div class="row marketing">
         <div class="col-lg-6">
-          <h4>Subheading</h4>
+          <h4>In 2+ sentences describe who you are and what you do</h4>
           <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
 
-          <h4>Subheading</h4>
+          <h4>Why are you taking this course?</h4>
           <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
 
-          <h4>Subheading</h4>
+          <h4>What previous experience do you have with programming?</h4>
           <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
         </div>
 
         <div class="col-lg-6">
-          <h4>Subheading</h4>
+          <h4>Are you working on a Mac, PC, or some other platform?</h4>
           <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
 
-          <h4>Subheading</h4>
+          <h4>Anything else we should know?</h4>
           <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
 
-          <h4>Subheading</h4>
+          <h4>Social Media</h4>
           <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
         </div>
       </div>
 
       <footer class="footer">
-        <p>&copy; Company 2014</p>
+        <p>&copy; Philip James Kelnhofer 2015</p>
       </footer>
 
     </div> <!-- /container -->
