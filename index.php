c93466e059fd4938a3993ec4bea41fcedcddacf5
PhilipJames <pjkelnhofer@gmail.com>
2015-09-13 13:27:13 -0400

    Add social media image files and bulk of content. Small styling tweaks.

--- index.php
+++ index.php
@@ -23,20 +23,12 @@
 
     <div class="container">
       <div class="header clearfix">
-        <nav>
-          <ul class="nav nav-pills pull-right"><!-- 
-            <li role="presentation" class="active"><a href="#">Home</a></li>
-            <li role="presentation"><a href="#">About</a></li>
-            <li role="presentation"><a href="#">Contact</a></li> -->
-          </ul>
-        </nav>
         <h3 class="text-muted">Philip Kelnhofer</h3>
       </div>
 
       <div class="jumbotron">
         <img class="img-responsive" src="img/ProfileImage960x720.jpeg" alt="picture of Philip Kelnhofer">
         <h1>DWA-15 Portfolio</h1>
-        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
       </div>
       <div class="row">
         <div class="col-lg-12 well">
@@ -52,24 +44,28 @@
       <div class="row marketing">
         <div class="col-lg-6">
           <h4>In 2+ sentences describe who you are and what you do</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+          <p>I have been working as a TV director in the Boston market for 20 years. Having seen technology change both how shows are made and how they are delivered to the customers in that time, I am back in school because I want to be on the front lines of the new technologies and not left beind.</p>
 
           <h4>Why are you taking this course?</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+          <p>I have some knowledge of HTML, CSS, and Javascript, but would like to understand more about the server side of web development. I had to learn a tiny bit of PHP for a Wordpress site in <a href="https://www.extension.harvard.edu/academics/courses/introduction-web-content-management-systems-site-development/24048">DGMD E-25</a>, and would like to learn a lot more.</p>
 
           <h4>What previous experience do you have with programming?</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+          <p>I programmed on my Commodore 64 as a child, but never was too serious with it. I had a concentration in CS as an undergrad (20 years ago), but my career took me away from the programming side of computers. I have taken <a href="https://www.extension.harvard.edu/academics/courses/introduction-computer-science-using-java-i/14289">CSCI E-10A</a> and <a href="https://www.extension.harvard.edu/academics/courses/introduction-computer-science-using-java-ii/24027">CSCI E-10B</a> at Harvard Extension School as part of Digital Media Arts Master's Program. </p>
         </div>
 
         <div class="col-lg-6">
           <h4>Are you working on a Mac, PC, or some other platform?</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+          <p>I am working on a MacBook Pro which is coming up on four years old and recently survived an entire cup of coffee being spilled on it (well everything but the optical drive survived). I am running OS X Yosemite 10.10.5, and do most of my browsing and testing in Chrome.</p>
 
           <h4>Anything else we should know?</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+          <p>Hmm... After not leaving North America for the first 36 years of my life, I made it to all seven continents between June 2011 and January 2015. I take a teddy bear made by my grandmother in 1967 on all of my trips since it was her dream to travel the world.</p>
 
           <h4>Social Media</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+          <span class="social-links">
+            <a href="https://twitter.com/pjkelnhofer"><img src="img/twitter.png"></a>
+            <a href="https://www.facebook.com/pjkelnhofer"><img src="img/facebook.png"></a>
+            <a href="https://www.linkedin.com/in/pjkelnhofer"><img src="img/linkedin.png"></a>
+          </span>  
         </div>
       </div>
 
